48d0b4aeb199d3dedda1581c15eada88529ed212
Anton Karmanov <a.karmanov@inventati.org>
2021-11-20 13:39:37 +0300

    Minor fixes

--- docker-monolith/start.sh
+++ docker-monolith/start.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-/usr/bin/mongod --fork --logpath /var/log/mongod.log --config /etc/mongodb.conf
+/usr/bin/mongod --fork --logpath /var/log/mongod.log --config /etc/mongod.conf
 
 # shellcheck source=/dev/null
 source "${REDDIT_DIR}/db_config"

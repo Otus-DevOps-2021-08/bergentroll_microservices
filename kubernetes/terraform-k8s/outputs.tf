d8b5bd246e4f82aea7c6c6544a26e1cdc70f6024
Anton Karmanov <a.karmanov@inventati.org>
2022-02-14 14:12:15 +0300

    Append Terraform template for node_group

--- kubernetes/terraform-k8s/outputs.tf
+++ kubernetes/terraform-k8s/outputs.tf
@@ -1,3 +1,7 @@
 output "cluster_endpoint" {
-  value = yandex_kubernetes_cluster.cluster.master.0.external_v4_endpoint
+  value = yandex_kubernetes_cluster.cluster.master[0].external_v4_endpoint
+}
+
+output "cluster_version_info" {
+  value = yandex_kubernetes_cluster.cluster.master[0].version_info[0]
 }
--- kubernetes/terraform-k8s/variables.tf
+++ kubernetes/terraform-k8s/variables.tf
@@ -6,54 +6,40 @@ variable "zone" { default = "ru-central1-a" }
 
 variable "service_account" {}
 
-variable "service_account_key_file" {
-  description = "YC secrets JSON file"
-}
+variable "service_account_key_file" { description = "YC secrets JSON file" }
 
 variable "k8s_version" { default = "1.19" }
 
 variable "network" { default = "default" }
 
-variable "cores_num" {
-  description = "Number of CPU cores"
+variable "node_group_size" { default = 2 }
+
+variable "node_cores_num" {
+  description = "Per node number of CPU cores"
   type        = number
   default     = 4
 }
 
-variable "core_fraction" {
-  description = "Assured vCPU time percent"
+variable "node_core_fraction" {
+  description = "Per node assured vCPU time percent"
   type        = number
   default     = 50
 }
 
-variable "memory" {
-  description = "RAM amount for an instance in GB"
+variable "node_memory" {
+  description = "Per node RAM amount for an instance in GB"
   type        = number
-  default     = 4
+  default     = 8
 }
 
-variable "disk_size" {
-  description = "Storage size in GB"
+variable "node_disk_size" {
+  description = "Per node storage size in GB"
   type        = number
-  default     = 40
+  default     = 64
 }
 
-variable "preemptible" {
-  description = "Allow the cloud to interrupt an instance"
+variable "node_preemptible" {
+  description = "Allow the cloud to interrupt nodes"
   type        = bool
   default     = false
-}
-
-variable "public_key_path" {
-  description = "Path to the public key used for SSH access"
-}
-
-variable "private_key_path" {
-  description = "Path to the private key used for SSH access"
-}
-
-variable "subnet_name" {}
-
-variable "inventory_output_dir" {
-  description = "Directory to put generated Ansible inventory file"
 }
